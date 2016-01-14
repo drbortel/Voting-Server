9174f3ba84246e6a0d974b15b66c7b627e26bf2a
Darren Bortel <drbortel@gmail.com>
2016-01-14 14:55:18 -0600

    Taking 2 entries under the Vote

--- src/core.js
+++ src/core.js
@@ -1,9 +1,17 @@
+import {List, Map} from 'immutable';
+
 export function setEntries(state, entries) {
     return state.set('entries', entries);
 }
 
-import {List} from 'immutable';
-
 export function setEntries(state, entries) {
     return state.set('entries', List(entries));
+}
+
+export function next(state) {
+    const entries = state.get('entries');
+    return state.merge({
+        vote: Map({pair: entries.take(2)}),
+        entries: entries.skip(2)
+    });
 }
--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
@@ -15,5 +14,31 @@ describe('application logic', () => {
                 entries: List.of('Trainspotting', '28 Days Later')
             }));
         });
+
+        it('converts to immutable', () => {
+            const state = Map();
+            const entries = ['Trainspotting', '28 Days Later'];
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        });
+    });
+
+    describe('next', () => {
+
+        it('takes the next two entries under vote', () => {
+            const state = Map({
+                entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later')
+                }),
+                entries: List.of('Sunshine')
+            }));
+        });
+
     });
 });
